ea3df73cf44dd5a42d3adc1c8eef0a08d16f5a18
Tamerlan <dzhangtim@list.ru>
2022-11-06 19:24:24 +0300

    new programm1

---
+++ problem19.2/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[5];
+int n = array.Length;
+for (int i = 0; i<(n/2); i++)
+{
+  if (array[i] == array[n-i-1] ){
+    Console.WriteLine(true);
+  }
+else {
+ Console.WriteLine(false);
+} 
+}
