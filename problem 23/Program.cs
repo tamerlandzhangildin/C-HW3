0e8129f0db36945b1940b113f3e66037d3ea2944
Tamerlan <dzhangtim@list.ru>
2022-11-06 19:52:52 +0300

    new programm4

---
+++ problem 23/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+double[] array = new double[n];
+
+for (int i = 0; i < n; i++)
+array[i] = Math.Pow(i + 1, 3);
+
+foreach (double i in array)
+Console.WriteLine(i);
