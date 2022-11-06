e9ed6495fa6c6db10499617a21a1814b46e697af
Tamerlan <dzhangtim@list.ru>
2022-11-06 19:12:03 +0300

    new programm

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[5];
+int n =array.Length;
+
+while (n < array.Length) 
+{ 
+ if ( n/1000 == n/10 && n%10000 == n%10 ) 
+ {
+    Console.WriteLine(true); 
+ }
+else{
+    Console.WriteLine(false);
+}
+ }
+ 
+
+
+
